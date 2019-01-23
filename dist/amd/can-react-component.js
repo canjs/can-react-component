37e934338724ce2fea098224b3cf946d3c6c3f3c
Ivo Pinheiro <ivo.pinheiro@nokia.com>
2019-01-23 20:02:42 +0000

    2.0.2

---
+++ dist/amd/can-react-component.js
@@ -0,0 +1,71 @@
+/*can-react-component@2.0.1#can-react-component*/
+define('can-react-component', [
+    'require',
+    'exports',
+    'module',
+    'react',
+    'can-namespace',
+    'can-assign',
+    'can-reflect',
+    'can-symbol'
+], function (require, exports, module) {
+    var React = require('react');
+    var namespace = require('can-namespace');
+    var assign = require('can-assign');
+    var reflect = require('can-reflect');
+    var canSymbol = require('can-symbol');
+    var viewModelSymbol = canSymbol.for('can.viewModel');
+    module.exports = namespace.reactComponent = function canReactComponent(displayName, CanComponent) {
+        if (arguments.length === 1) {
+            CanComponent = arguments[0];
+            displayName = (CanComponent.shortName || 'CanComponent') + 'Wrapper';
+        }
+        function Wrapper() {
+            React.Component.call(this);
+            this.canComponent = null;
+            this.createComponent = this.createComponent.bind(this);
+        }
+        Wrapper.displayName = displayName;
+        Wrapper.prototype = Object.create(React.Component.prototype);
+        assign(Wrapper.prototype, {
+            constructor: Wrapper,
+            createComponent: function (el) {
+                if (this.canComponent) {
+                    this.canComponent = null;
+                }
+                if (el) {
+                    this.vm = el[viewModelSymbol];
+                    if (this.vm) {
+                        reflect.assign(this.vm, this.props);
+                    } else {
+                        this.canComponent = new CanComponent(el, { viewModel: this.props });
+                        this.vm = this.canComponent.viewModel;
+                    }
+                }
+            },
+            componentDidUpdate: function () {
+                this.vm && reflect.assign(this.vm, this.props);
+            },
+            render: function () {
+                return React.createElement(CanComponent.prototype.tag, { ref: this.createComponent });
+            }
+        });
+        Object.defineProperty(Wrapper.prototype, 'viewModel', {
+            enumerable: false,
+            configurable: true,
+            get: function () {
+                return this.vm;
+            }
+        });
+        try {
+            Object.defineProperty(Wrapper, 'name', {
+                writable: false,
+                enumerable: false,
+                configurable: true,
+                value: displayName
+            });
+        } catch (e) {
+        }
+        return Wrapper;
+    };
+});
